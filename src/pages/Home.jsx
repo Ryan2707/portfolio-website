12b6b7f613d64335881d84a79d477182978f3941
Ryan <9024640@student.zadkine.nl>
2025-11-04 15:45:03 +0100

    img

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,16 @@
+import './Home.css'
+import picture from '../assets/20251104_150236.jpg'
 const Home = () => {
     return (
-        <div></div>
+        <div>
+        <section className="Left">
+            <h1>Welcome to My Portfolio</h1>
+        </section>
+
+        <section className="Right">
+        <img className="img" src={picture} alt="" />
+        </section>
+        </div>
     );
 }
 
